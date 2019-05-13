6d6892681abb62498e4cc9cc71aef1cdfd807aa2
wangzhipeng <wangzhipeng@speakin.mobi>
2019-05-13 15:11:37 +0800

    [2] add the gosu into image

---
+++ build/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+#gosu root chmod +066 $HOME/workspace -R
+exec -c gosu gopher $@
--- go_vim.sh
+++ go_vim.sh
@@ -4,6 +4,7 @@ volume_root="go_vim_root:/root"
 volume_usr="go_vim_usr:/usr"
 volume_var="go_vim_var:/var"
 volume_etc="go_vim_etc:/etc"
+
 network=host
 image="luckydogchian/go_vim:latest"
 workdir="/home/gopher"
@@ -12,9 +13,7 @@ name="go_vim"
 gopath=$1
 localdir=$2
 
-#docker run -v $volume_root -v $volume_usr -v $volume_var -v $volume_etc \
 docker run -v $volume_root -v $volume_usr -v $volume_etc -v $volume_var\
        	-v $gopath:/gopath -v $localdir:$workdir/workspace --network $network --name $name \
-	-u 1040:1030 --workdir $workdir/workspace $image bash -c "tail -f"
-       	#--workdir $workdir --privileged $image bash -c "tail -f"
+	--rm --workdir $workdir/workspace $image 
 
